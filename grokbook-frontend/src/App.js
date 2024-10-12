fa605bf1e8ce35db7d5bf77ffa2d4aa045cac1a7
daemon1ze <132319012+daemon1ze@users.noreply.github.com>
2024-10-12 15:56:47 -0700

    feat: working darkmode

--- grokbook-frontend/src/App.js
+++ grokbook-frontend/src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import AppRoutes from './routes';
 import Header from './components/Header';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <AppRoutes />
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <div className="App">
+          <Header toggleColorMode={toggleColorMode} />
+          <AppRoutes />
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
